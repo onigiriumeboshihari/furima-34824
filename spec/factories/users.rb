6172acc881e0f019e7b2ef72b5b2fe391a2479b9
onigiriumeboshihari <koyamada89179@gol.com>
2021-03-23 13:44:50 +0900

    ユーザー管理機能（Faker更新）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6, mix_case: true) }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     last_name             {'名字'}
     first_name            {'名前'}
