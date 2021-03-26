b901684f78d6eead0d1c42ff95165e893489326e
onigiriumeboshihari <koyamada89179@gol.com>
2021-03-26 13:29:33 +0900

    Update calc.js

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -1,7 +1,8 @@
 function calc() {
   const itemPrice = document.getElementById("item-price")
   itemPrice.addEventListener("keyup", () => {
-    const calcAddTaxPrice = itemPrice.value / 10;
+    const beforeAddTaxPrice = itemPrice.value / 10;
+    const calcAddTaxPrice = Math.trunc(beforeAddTaxPrice);
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${calcAddTaxPrice}`;
     const calcProfit = `${itemPrice.value - calcAddTaxPrice}`;
